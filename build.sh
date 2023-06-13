ea63fd51e7f4d57a19b14f2119cac92151108104
syusaku625 <maedasyu625@gmail.com>
2023-06-13 21:43:43 +0900

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build
 cd build
 cmake -D EIGEN_DIR=/mnt/d/lib/eigen/include/eigen3\
-      -D CMAKE_INSTALL_PREFIX=/mnt/d/work/stokes_flow \
+      -D CMAKE_INSTALL_PREFIX=/mnt/d/work/stokes_flow/bin \
       -D TP_DIR=/mnt/d/lib/TextParser \
       ..
 make && make install
